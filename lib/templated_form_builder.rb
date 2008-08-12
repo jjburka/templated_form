28fdf72474a20ab4c2a6c3d3c1a507872cc73650
James Burka <jjburka@burkabook.local>
2008-08-11 22:24:01 -0400

    examples

--- lib/templated_form_builder.rb
+++ lib/templated_form_builder.rb
@@ -1,9 +1,15 @@
 #modified form Advanced Rails Recipes
 
+
 class TemplatedFormBuilder < ActionView::Helpers::FormBuilder
+  
+  
+  #displays the error message for the field
   def error_message(field,options)
     errors = object.errors.on(field)
     message = String.new
+    
+    # if the label has not been expressly set use the field name
     message = "#{(options[:label] || field).to_s.humanize} " unless field == :base
     "#{message}#{errors.is_a?(Array) ? errors.to_sentence : errors}"
   end
@@ -11,7 +17,7 @@ class TemplatedFormBuilder < ActionView::Helpers::FormBuilder
   helpers = field_helpers +
     %w(date_select datetime_select time_select collection_select) +
     %w(collection_select select country_select time_zone_select)  -
-    %w(label fields_for hidden_field)
+    %w(label fields_for hidden_field) #these are skipped over
   
 
   helpers.each do |name|
@@ -27,6 +33,12 @@ class TemplatedFormBuilder < ActionView::Helpers::FormBuilder
   
   def build_shell(field,options)
     @template.capture do
+      
+      #Settings
+      #:use_base - will only display the base error message set by the model
+      #:label - can be anything the normal label method will take
+                #:label=>["My Label",{:class=>"inline"}]
+                #:label=>"My Label"
       template_options = {:field=>(options.delete(:use_base) ? :base : field),
                           :label=>options.delete(:label)}
       locals = { 
