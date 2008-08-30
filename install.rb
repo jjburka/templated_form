4f680adfcc3aebb59df981a8647e2d87aee66ba4
James Burka <jjburka@burkabook.local>
2008-08-30 13:09:58 -0400

    fixed installer

--- install.rb
+++ install.rb
@@ -1,33 +1,21 @@
 require 'fileutils'
 
+
+#RAILS_ROOT = File.join(File.dirname(__FILE__),'..','test')
+
 PARTIAL_PATH = File.join(RAILS_ROOT ,'app' ,'views' , 'forms')
 PLUGIN_ROOT = File.dirname(__FILE__)
 
-FileUtils.mkdir_p install_to
-File.open(File.join(PARTIAL_PATH,'_field.html.erb'), 'w') do |f| 
-  f.write(
-<<-EOF
-<p>
-  <%= label %>
-  <%= element %>
-</p>    
-EOF
-  )
-end
+FileUtils.mkdir_p PARTIAL_PATH
 
-File.open(File.join(PARTIAL_PATH,'_field_with_error.html.erb'), 'w') do |f| 
-  f.write(
-<<-EOF
-<p class="field_with_error">
-  <%= label %>
-  <%= element %>
-  <span class="error_message">
-    <%= error %>
-  </span>
-</p>   
-EOF
-  )
-end
+field = '_field.html.erb'
+FileUtils.copy(File.join(PLUGIN_ROOT,'resources',field),
+  File.join(PARTIAL_PATH,field_with_error))
+
+field_with_error = '_field_with_error.html.erb'
+FileUtils.copy(File.join(PLUGIN_ROOT,'resources',field_with_error),
+  File.join(PARTIAL_PATH,field_with_error))
 
 watermark = 'jquery.watermark.js'
-File.cp(File.join(PLUGIN_ROOT,'resources',watermark),File.join(RAILS_ROOT,'public','javascripts',watermark))
+FileUtils.copy(File.join(PLUGIN_ROOT,'resources',watermark),
+  File.join(RAILS_ROOT,'public','javascripts',watermark))
